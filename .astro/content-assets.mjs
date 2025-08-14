e9fa3f103172e273ed52b20ac4921fff642c8dd5
Yousif Abood <yousif@yabood.com>
2025-08-14 07:23:53 -0400

    Fix build issues

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,6 +1,6 @@
 
-import __ASTRO_IMAGE_IMPORT_oUbH0 from "../../assets/hero-astro-blog.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fhello-world.mdx";
 import __ASTRO_IMAGE_IMPORT_ZYDeAy from "../../assets/astro-performance.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fastro-performance-guide.mdx";
 import __ASTRO_IMAGE_IMPORT_rXmwy from "../../assets/wide-landscape.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fcreative-image-layouts.mdx";
-export default new Map([["../../assets/hero-astro-blog.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fhello-world.mdx", __ASTRO_IMAGE_IMPORT_oUbH0], ["../../assets/astro-performance.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fastro-performance-guide.mdx", __ASTRO_IMAGE_IMPORT_ZYDeAy], ["../../assets/wide-landscape.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fcreative-image-layouts.mdx", __ASTRO_IMAGE_IMPORT_rXmwy]]);
+import __ASTRO_IMAGE_IMPORT_oUbH0 from "../../assets/hero-astro-blog.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fhello-world.mdx";
+export default new Map([["../../assets/astro-performance.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fastro-performance-guide.mdx", __ASTRO_IMAGE_IMPORT_ZYDeAy], ["../../assets/wide-landscape.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fcreative-image-layouts.mdx", __ASTRO_IMAGE_IMPORT_rXmwy], ["../../assets/hero-astro-blog.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fhello-world.mdx", __ASTRO_IMAGE_IMPORT_oUbH0]]);
 		
--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,6 @@
 
 export default new Map([
-["src/content/blog/hello-world.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhello-world.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/astro-performance-guide.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fastro-performance-guide.mdx&astroContentModuleFlag=true")],
-["src/content/blog/creative-image-layouts.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fcreative-image-layouts.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/creative-image-layouts.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fcreative-image-layouts.mdx&astroContentModuleFlag=true")],
+["src/content/blog/hello-world.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhello-world.mdx&astroContentModuleFlag=true")]]);
 		
