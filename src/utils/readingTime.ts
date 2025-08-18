2ca50091a4223f6ef1a00b907255f17e3c7dfaf7
Yousif Abood <yousif@yabood.com>
2025-08-17 22:22:21 -0400

    Reading time and other changes

---
+++ src/utils/readingTime.ts
@@ -0,0 +1,11 @@
+export function calculateReadingTime(content: string): string {
+  const wordsPerMinute = 200;
+  const words = content.trim().split(/\s+/).length;
+  const minutes = Math.ceil(words / wordsPerMinute);
+  
+  if (minutes === 1) {
+    return '1 min read';
+  } else {
+    return `${minutes} min read`;
+  }
+}
