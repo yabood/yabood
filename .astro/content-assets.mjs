2baa06f14b181dbaf56396f66190dbb0a199020c
Yousif Abood <yousif@yabood.com>
2025-08-18 11:16:21 -0400

    Make all posts draft

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,7 +1,7 @@
 
-import __ASTRO_IMAGE_IMPORT_2p16uR from "../../assets/hero-astro-blog.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fdraft-example.mdx";
+import __ASTRO_IMAGE_IMPORT_ZYDeAy from "../../assets/astro-performance.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fastro-performance-guide.mdx";
 import __ASTRO_IMAGE_IMPORT_oUbH0 from "../../assets/hero-astro-blog.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fhello-world.mdx";
 import __ASTRO_IMAGE_IMPORT_rXmwy from "../../assets/wide-landscape.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fcreative-image-layouts.mdx";
-import __ASTRO_IMAGE_IMPORT_ZYDeAy from "../../assets/astro-performance.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fastro-performance-guide.mdx";
-export default new Map([["../../assets/hero-astro-blog.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fdraft-example.mdx", __ASTRO_IMAGE_IMPORT_2p16uR], ["../../assets/hero-astro-blog.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fhello-world.mdx", __ASTRO_IMAGE_IMPORT_oUbH0], ["../../assets/wide-landscape.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fcreative-image-layouts.mdx", __ASTRO_IMAGE_IMPORT_rXmwy], ["../../assets/astro-performance.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fastro-performance-guide.mdx", __ASTRO_IMAGE_IMPORT_ZYDeAy]]);
+import __ASTRO_IMAGE_IMPORT_2p16uR from "../../assets/hero-astro-blog.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fdraft-example.mdx";
+export default new Map([["../../assets/astro-performance.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fastro-performance-guide.mdx", __ASTRO_IMAGE_IMPORT_ZYDeAy], ["../../assets/hero-astro-blog.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fhello-world.mdx", __ASTRO_IMAGE_IMPORT_oUbH0], ["../../assets/wide-landscape.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fcreative-image-layouts.mdx", __ASTRO_IMAGE_IMPORT_rXmwy], ["../../assets/hero-astro-blog.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fdraft-example.mdx", __ASTRO_IMAGE_IMPORT_2p16uR]]);
 		
